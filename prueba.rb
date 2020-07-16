af1a841bb092bf9ea68eb49d7013586896a3be9e
dgarate <garatex@gmail.com>
2020-07-16 02:15:48 -0400

    sending ruby exam... MIRSON/SEBA MIS DISCULPAS por el atraso profes pero estoy a tope con el trabajo y no pude antes, espero lo puedan entender

---
+++ prueba.rb
@@ -0,0 +1,43 @@
+require "uri"
+require "net/http"
+require 'json'
+
+
+def request(address)
+
+url = URI(address)
+
+https = Net::HTTP.new(url.host, url.port);
+https.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+
+response = https.request(request)
+
+JSON.parse response.read_body
+end
+
+hash_resultados = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=TjnnHaVZy702voK5eFhMRidCvsnLLsABPzK7SJRV")
+
+images = hash_resultados["photos"]
+
+
+def build_web_page (images)
+
+
+images.each do |i|
+    i.each do |k , v|
+    output = "<html>\n<head>\n</head>\n\t<body>\n\t\t<ul>"
+        if k == "img_src"
+            output += "\n\t\t\t<li><img src=\"#{v}\"> </li>\n\t\t</ul>\n\t</body>\n</html>"
+            File.write('index.html', output)
+        end
+        end
+    end
+end
+
+build_web_page(images)
+
+
+
+
